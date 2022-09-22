4d248ccfb3c280c492b29d810aa9458e2bd1a4ac
valdesius <v.inc@inbox.ru>
2022-09-22 09:25:55 +0300

    version 1

--- src/valdes/com/company/MainWindow.java
+++ src/valdes/com/company/MainWindow.java
@@ -1,15 +1,15 @@
 package valdes.com.company;
 
 import javax.swing.*;
-import java.awt.*;
 
-public class MainWindow extends JFrame {
-    private DrawPanel dp;
-    public MainWindow() throws HeadlessException {
-        dp = new DrawPanel();
-        add(dp);
-
-
+public class MainWindow {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        DrawingCanvas drawingCanvas = new DrawingCanvas(800, 600);
+        frame.setSize(900, 700);
+        frame.setTitle("Aquarium");
+        frame.add(drawingCanvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
-
 }
